147957cc0564e3fef91928ca9aa3b36cda059ee2
Ashley Christie <achristie0319@gmail.com>
2021-06-20 22:47:57 -0400

    Co-authored-by: ambersu123 <ambersu123@users.noreply.github.com> Co-authored-by: Paolo Urciullo <paolourciullo@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const form = document.getElementById('loginForm')
+const form = document.getElementById("loginForm");
+const calendar = document.getElementById("");
 
 function check(form) {
   if (
-    form.email.value == '123@xyz.com' &&
-    form.password.value == 'toucans31!'
+    form.email.value == "123@xyz.com" &&
+    form.password.value == "toucans31!"
   ) {
-    window.open('about.html')
-    console.log(form.email.value)
+    window.open("about.html");
+    console.log(form.email.value);
   } else {
     alert(
-      'There is something wrong with your password/email.  Click the cancel button to try again'
-    )
+      "There is something wrong with your password/email.  Click the cancel button to try again"
+    );
   }
 }
