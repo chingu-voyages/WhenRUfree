dc9be36aaafdd691bf7ec42b4f1428e18cbc4c83
Ashley Christie <achristie0319@gmail.com>
2021-06-24 19:45:08 -0400

    Co-authored-by: Huda-cloud-27598 <Huda-cloud-27598@users.noreply.github.com> Co-authored-by: Paolo Urciullo <paolourciullo@users.noreply.github.com>

--- script.js
+++ script.js
@@ -6,3 +6,22 @@
     x.className = "topnav";
   }
 }
+
+function myFunction() {
+  if (document.getElementById("calender")) {
+    if (document.getElementById("calender").style.display == "none") {
+      document.getElementById("calender").style.display = "block";
+      document.getElementById("back").style.display = "none";
+    } else {
+      document.getElementById("calender").style.display = "none";
+      document.getElementById("back").style.display = "block";
+    }
+  }
+}
+
+function closeFunction() {
+  document.getElementById("back").style.display = "none";
+  document.getElementById("calender").style.display = "block";
+}
+/* flip animation toggle
+Dismiss button returns to calender */
