a753fd34cf08f3496c7a087bd7e5c29047981b97
Ashley Christie <achristie0319@gmail.com>
2021-07-11 20:40:16 -0400

    fixed hamburger menus login and index.html

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 ﻿function myFunction_3() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+/*function myFunction_3() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
-}
+}*/
 
 function myFunction_1() {
   if (document.getElementById("calender")) {
